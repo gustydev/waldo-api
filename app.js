0d2a5a3407db72a7277a0f4ee739795eed31f963
gusty <gustavodiorlando@gmail.com>
2024-09-17 15:28:40 -0300

    Add error handling, CORS and change images to jpeg
    Because I like jpegs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const createError = require('http-errors');
+const cors = require('cors');
+
 const mapRouter = require('./routes/map')
 const gameRouter = require('./routes/game')
 
@@ -29,12 +32,33 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+app.use(cors()); // Later change to only allow the game website!!
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 app.use('/api/map', mapRouter)
 app.use('/api/game', gameRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(statusCode).json({
+      errors: {
+          messages: [message],
+          statusCode,
+          stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+      }
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`App listening on port ${port}`)})
